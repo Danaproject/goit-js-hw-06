7f6a5933ee949f8fe859eed8f2eed76dcf780116
Dana Project <70840490+Danaproject@users.noreply.github.com>
2020-12-19 23:19:41 +0100

    + task 1-6 shortened

--- js/tasks.js
+++ js/tasks.js
@@ -2,59 +2,48 @@ import users from './users.js'
 
 // ------------ Задание 1 ----------------------------------------------
 
-const getUserNames = users => {
-    return users.map(user => user.name);
-  };
-  console.log(getUserNames(users));
-  // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+const getUserNames = users => users.map(user => user.name);
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 // ------------ Задание 2 ----------------------------------------------
 
-const getUsersWithEyeColor = (users, color) => {
-    return users.filter(user => user.eyeColor === color);
-  };
-  console.log(getUsersWithEyeColor(users, 'blue')); 
-  // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+console.log(getUsersWithEyeColor(users, 'blue')); 
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 // ------------ Задание 3 ----------------------------------------------
 
 const getUsersWithGender = (users, gender) => {
     const usersWithGender = users.filter(user => user.gender === gender);
     return usersWithGender.map(userWithGender => userWithGender.name);
-  };
-  console.log(getUsersWithGender(users, 'male')); 
-  // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+};
+console.log(getUsersWithGender(users, 'male')); 
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 // ------------ Задание 4 ----------------------------------------------
 
-const getInactiveUsers = users => {
-    return users.filter(user => !user.isActive)
-  };
-  console.log(getInactiveUsers(users)); 
-  // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+const getInactiveUsers = users => users.filter(user => !user.isActive);
+console.log(getInactiveUsers(users)); 
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 
 // ------------ Задание 5 ----------------------------------------------
 
-const getUserWithEmail = (users, email) => {
-    return users.find(user => user.email === email);
-  };
-  
-  console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); 
-  // {объект пользователя Sheree Anthony}
-  console.log(getUserWithEmail(users, 'elmahead@omatom.com')); 
-  // {объект пользователя Elma Head}
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); 
+// {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); 
+// {объект пользователя Elma Head}
 
 // ------------ Задание 6 ----------------------------------------------
 
-const getUsersWithAge = (users, min, max) => {
-    return users.filter(user => min < user.age && user.age < max);
-  };
-  
-  console.log(getUsersWithAge(users, 20, 30)); 
-  // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
-  
-  console.log(getUsersWithAge(users, 30, 40));
-  // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+const getUsersWithAge = (users, min, max) => users.filter(user => min < user.age && user.age < max);
+ 
+console.log(getUsersWithAge(users, 20, 30)); 
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 // ------------ Задание 7 ----------------------------------------------
